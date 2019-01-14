a4484216a7aba86e3f90332bf062b0cd764a4e5b
Linn Strand <linn.strand@knowit.se>
2019-01-14 14:31:42 +0100

    working mongoose and express

--- my-server/api/index.js
+++ my-server/api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-require('./routes/user', router)
-require('./routes/workout', router)
+require('./routes/user')(router)
+require('./routes/workout')(router)
 
 module.exports = router
